4f1bf58ba230bf7dbcce42ecc7ea02ca76f1d90d
Michael Pashkov <mvpashkov@yandex.ru>
2018-11-01 16:02:37 +0300

    add animation to other pages

--- js/main.js
+++ js/main.js
@@ -2,7 +2,16 @@
 // Let's create a new file in the JS folder, named main.js
 //and add our programming.
 
-$(".animsition").animsition();
+$(".animsition").animsition({
+    inClass: 'fade-in-right-lg',
+    outClass: 'fade-out-right-lg',
+    inDuration: 1000, //1 sec
+    outDuration: 500,
+    //linkElement: 'header a:not([href^="#"])', // meaning any links except ones that start with a #
+    linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+//http://git.blivesta.com/animsition/
+
+});
 
 
 //First, we select the outer div for the page, the one we added the class name to.
